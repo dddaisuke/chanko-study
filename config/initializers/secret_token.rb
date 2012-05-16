f9de0459bfeb72a9b3c92b32236e4b3dc0dd28bd
Daisuke Yamashita <dddaisuke@gmail.com>
2012-05-17 00:29:34 +0900

    new step1.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Donabe::Application.config.secret_token = '5166d7dbac2d94cd0b8c680934c2538d1c77330c8fb075b237234d8280b417df1577b66d7e40b221514fae5e8fd514c6c766c59b61d40d45aa73a51bb1c1dc37'

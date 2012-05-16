9acfa2c10320fdd252199bd807d8f70a3912f5c2
Daisuke Yamashita <dddaisuke@gmail.com>
2012-05-17 01:18:54 +0900

    step12

--- app/controllers/mizu_controller.rb
+++ app/controllers/mizu_controller.rb
@@ -1,8 +1,9 @@
 class MizuController < ApplicationController
   def index
     @cal_answer = 1
-    invoke :konbu_ext, :ajitsuke
-    @cal_answer += 2
-    @cal_answer += 3
+    invoke :konbu_ext, :ajitsuke do
+      @cal_answer += 2
+      @cal_answer += 3
+    end
   end
 end
